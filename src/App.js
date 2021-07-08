a2bb2fba1535981466b1cb8e183f90a107d93529
CarloseRochav <78558271+CarloseRochav@users.noreply.github.com>
2021-07-08 14:05:41 -0700

    Formulario prototipo

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+//import Button from 'react-bootstrap/Button'//Usar el componen de react-bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css'//Estilo desde bootstrap-css
+import {Button,Form, Container} from 'react-bootstrap' //importar estos componentes desde bootstrap
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Container>
+          <Form>
+            <Form.Group>
+              <Form.Label>N o m b r e</Form.Label>
+              <Form.Control type="name" placeholder="Ingrese su primero nombre" />
+              <Form.Text className="text-muted">
+                Importante ingrese su primer nombre, por favors
+              </Form.Text>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>A p e l l i d o</Form.Label>
+              <Form.Control type="name" placeholder="Ingrese su primer apellido "/>
+              <Form.Text className="text-muted">
+                Importante ingrese un apellido, por favor
+              </Form.Text>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Correo Electronico</Form.Label>
+              <Form.Control type="email" placeholder="Ejemplo@email.com"/>
+              <Form.Text className="text-muted">
+                Ingrese un correo electronico
+              </Form.Text>
+            </Form.Group>
+          </Form>
+        </Container>
+        
+        <Button>Boton de enviar</Button>
+       
       </header>
     </div>
   );
